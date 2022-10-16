f7f4b7fe00c233048f106cf7f25bf4f48833b0d9
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-16 18:14:23 +0800

    Added the manyUserFinal

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,9 +1,11 @@
 const { newUser } = require('./createUser')
 const { user } = require('./getUser')
 const { userFinal } = require('./updateUser')
+const { manyUserFinal } = require('./updateManyUser')
 
 module.exports = {
   user,
   userFinal,
-  newUser
+  newUser,
+  manyUserFinal,
 }
