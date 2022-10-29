c79c4a8b1bb97d197d84fe8a728e6015ebd5ffb2
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-29 21:26:39 +0800

    Updated Conroller

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,13 +1,16 @@
 const { newUser } = require('./createUser')
 const { user } = require('./getUser')
-const { userFinal } = require('./updateUser')
+const { userFinal, updateUser } = require('./updateUser')
 const { manyUserFinal } = require('./updateManyUser')
 const { getProfile } = require('./getProfile')
+const { viewUser } = require('./viewUser')
 
 module.exports = {
   user,
   userFinal,
   newUser,
   manyUserFinal,
-  getProfile
+  getProfile,
+  viewUser,
+  updateUser
 }
