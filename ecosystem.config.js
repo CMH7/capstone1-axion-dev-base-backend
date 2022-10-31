43b41e3536af7b7e2b4cbb6b95e51d36d671f677
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-11-01 05:34:41 +0800

    Removed unknown service worker file and directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   apps : [{
     script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+    watch: '.',
   }],
 
   deploy : {
