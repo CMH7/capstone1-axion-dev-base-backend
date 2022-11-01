b5535fe31cb8607056492f6bf30119b9ac3d2a03
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-11-01 15:18:31 +0800

    self catch of error

--- controllers/user/updateManyUser.js
+++ controllers/user/updateManyUser.js
@@ -1,38 +1,45 @@
-const { prisma } = require('../../constants')
+const { prisma, log } = require('../../constants')
 
 module.exports = {
-  manyUserFinal: async (/** @type account[] */ accounts) => {
-    let trs = []
-		accounts.forEach(account => {
-      trs = [
-				...trs,
-				prisma.accounts.update({
-					where: {
-						id: account.id,
-					},
-					data: {
-						invitations: account.invitations,
-						subjects: account.subjects,
-						notifications: account.notifications,
-						age: account.age,
-						course: account.course,
-						email: account.email,
-						firstName: account.firstName,
-						gender: account.gender,
-						lastName: account.lastName,
-						password: account.password,
-						profile: account.profile,
-						school: account.school,
-						useHint: account.useHint,
-						year: account.year,
-						lastActive: new Date().toISOString(),
-						bio: account.bio,
-						verified: account.verified,
-					},
-				}),
-			];
-    })
+	manyUserFinal: async (/** @type account[] */ accounts) => {
+		try {
+			let trs = [];
+			accounts.forEach((account) => {
+				trs = [
+					...trs,
+					prisma.accounts.update({
+						where: {
+							id: account.id,
+						},
+						data: {
+							invitations: account.invitations,
+							subjects: account.subjects,
+							notifications: account.notifications,
+							age: account.age,
+							course: account.course,
+							email: account.email,
+							firstName: account.firstName,
+							gender: account.gender,
+							lastName: account.lastName,
+							password: account.password,
+							profile: account.profile,
+							school: account.school,
+							useHint: account.useHint,
+							year: account.year,
+							lastActive: new Date().toISOString(),
+							bio: account.bio,
+							verified: account.verified,
+						},
+					}),
+				];
+			});
 
-    return await prisma.$transaction(trs)
+			return await prisma.$transaction(trs)
+		} catch (e) {
+			log('!!! Retrying finalization updates')
+			const result = await this.manyUserFinal(accounts)
+			log('!!! Retrying finalization updates done')
+			return result
+		}
   }
 }
